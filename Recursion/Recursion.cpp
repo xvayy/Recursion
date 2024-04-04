1a140553663eeb2c538a5ae66feec75188c9b8cb
Yuliia <ivanykjula@gmail.com>
2024-04-04 22:10:23 +0300

    Add Geometric Progression

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -1,132 +1,124 @@
 ﻿#include <iostream>
 using namespace std;
 
-// Tail recursion to calculate the sum of array elements
-int tailRecursiveSumOfProg(int arr[], int size, int index = 0, int currentSum = 0) {
-    if (index >= size) return currentSum;
-    return tailRecursiveSumOfProg(arr, size, index + 1, currentSum + arr[index]);
-}
-
-// Linear recursion to calculate the sum of array elements
-int linearRecursiveSumOfProg(int arr[], int size) {
-    if (size == 0) return 0;
-    return arr[size - 1] + linearRecursiveSumOfProg(arr, size - 1);
+// Linear recursion to calculate the sum of digits of a number(Task 3)
+int sumOfDigits(int num) {
+    if (num == 0)
+        return 0;
+    else
+        return num % 10 + sumOfDigits(num / 10);
 }
 
 
-void merge(int arr[], int start, int middle, int end) {
-    int i = start;
-    int j = middle + 1;
-    int k = start;
-
-    // Dynamically allocate memory for a temporary array
-    int* temp = new int[end - start + 1];
-
-    // Merge two subarrays into a temporary array
-    while (i <= middle && j <= end) {
-        if (arr[i] >= arr[j]) {
-            temp[k - start] = arr[i];
-            i++;
-        }
-        else {
-            temp[k - start] = arr[j];
-            j++;
-        }
-        k++;
+int sumOfProg(unsigned long long int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
     }
-
-    // Add remaining elements of the first subarray, if any
-    while (i <= middle) {
-        temp[k - start] = arr[i];
-        i++;
-        k++;
-    }
-
-    // Add remaining elements of the second subarray, if any
-    while (j <= end) {
-        temp[k - start] = arr[j];
-        j++;
-        k++;
-    }
-
-    // Copy the contents of the temporary array back into the main array
-    for (int p = start; p <= end; p++) {
-        arr[p] = temp[p - start];
-    }
-
-    // Free the memory allocated for the temporary array
-    delete[] temp;
+    return sum;
 }
 
-void mergeSort(int arr[], int start, int end) {
-    if (start < end) {
-        int middle = (start + end) / 2;
-        mergeSort(arr, start, middle);
-        mergeSort(arr, middle + 1, end);
-        merge(arr, start, middle, end);
-    }
+// Tail recursion to calculate the sum of array elements (GEOMETRIC)
+int tailRecursion(int a1, int r, int i) {
+    if (i == 0)
+        return a1;
+    else
+        return r * tailRecursion(a1, r, i - 1);
+}
+
+// Linear recursion to calculate the sum of array elements (ARITHMETIC)
+int linearRecursion(int a1, int d, int i) {
+    if (i == 0)
+        return a1;
+    else
+        return d + linearRecursion(a1, d, i - 1);
 }
 
 int main() {
     setlocale(LC_ALL, "English");
-    cout << "Merge Sort\n";
-    int firstTerm, difference, N;
-
-    cout << "Enter the first element of the array: ";
+    int firstTerm, difference, denominator, N, result1, result2;
+    cout << "ARITHMETIC PROGRESSION\n";
+    cout << "Enter the first element of the progression: ";
     cin >> firstTerm;
 
     cout << "Enter the difference: ";
     cin >> difference;
 
-    cout << "Enter the size of the array (100 < N < 1000): ";
+    cout << "Enter the size of the progression (100 < N < 1000): ";
     cin >> N;
 
     // Checking the correctness of the entered data
     if (N <= 100 || N >= 1000) {
-        cout << "Incorrect array size!" << endl;
+        cout << "Incorrect progression size!" << endl;
         cin >> N;
     }
 
-    int* myarr;
-    myarr = new int[N];
+    unsigned long long int* myarr;
+    myarr = new unsigned long long int[N];
 
-    // Fill the array with members of the arithmetic progression
+    // Fill the array with members of the arithmetic progression()
     for (int i = 0; i < N; ++i) {
-        myarr[i] = firstTerm + i * difference;
+        myarr[i] = linearRecursion(firstTerm, difference, i);
     }
-    cout << "\nInitial array\n";
+    cout << "\nArithmetic progression(Linear recursion)\n\n";
     for (int i = 0; i < N; i++)
     {
         cout << myarr[i] << " ";
     }
 
-    int summ = tailRecursiveSumOfProg(myarr, N);
-    int summ2 = linearRecursiveSumOfProg(myarr, N);
+    result1 = sumOfProg(myarr, N);
+    cout << "\nSum of arithmetic progression: " << result1;
 
-    mergeSort(myarr, 0, N - 1);
-
-    cout << "\nSorted array in reverse order\n";
-    for (int i = 0; i < N; i++)
-    {
-        cout << myarr[i] << " ";
+    cout << "\n\nGEOMETRIC PROGRESSION\n";
+    cout << "Enter the first element of the progression: ";
+    cin >> firstTerm;
+    if (firstTerm == 0) {
+        while (firstTerm == 0) {
+            cout << "First element can not be 0" << endl;
+            cin >> firstTerm;
+        }
     }
 
-    cout << "\nSum of the arithmetic progression (Tail recursion)\n";
-    cout << summ;
-    cout << "\nSum of the arithmetic progression (Linear recursion)\n";
-    cout << summ2;
+    cout << "Enter the denominator: ";
+    cin >> denominator;
 
+    if (denominator == 0) {
+        while (denominator == 0) {
+            cout << "Denominator can not be 0" << endl;
+            cin >> denominator;
+        }
+    }
+
+    cout << "Enter the size of the progression (2 < N < 32): ";
+    cin >> N;
+
+    if (N <= 2 || N >= 32) {
+        cout << "Incorrect progression size!" << endl;
+        cin >> N;
+    }
+
+    unsigned long long int* myarr2;
+    myarr2 = new unsigned long long int[N];
+
+    // Fill the array with members of the geometric progression()
+    for (int i = 0; i < N; ++i) {
+        myarr2[i] = tailRecursion(firstTerm, denominator, i);
+    }
+    cout << "\nGeometric progression (Tail recursion)\n\n";
+    for (int i = 0; i < N; i++)
+    {
+        cout << myarr2[i] << " ";
+    }
+
+    result2 = sumOfProg(myarr2, N);
+    cout << "\nSum of geometric progression: " << result2;
 
     cout << "\n\nTask 3. Find the sum of the digits of a given natural number\n";
     int num;
-    std::cout << "Enter a number: ";
-    std::cin >> num;
+    cout << "Enter a number: ";
+    cin >> num;
 
-    int sum = 0;
-    while (num != 0) {
-        sum += num % 10;
-        num /= 10;
-    }
+    int sum = sumOfDigits(num);
 
-    std::cout << "\nSum of the digits of the number: " << sum << std::endl;
+    cout << "\nSum of the digits of the number: " << sum << std::endl;
 }
